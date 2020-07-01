cd1bc2418eefaf3f185add86177d26f8d39c17db
geewhombee <geewhombee@gmail.ca>
2020-06-30 18:34:50 -0600

    first commit

---
+++ app/src/main/java/ca/nait/dmit2504/finalprojectnm/MainActivity.java
@@ -0,0 +1,15 @@
+package ca.nait.dmit2504.finalprojectnm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String test = "test";
+    }
+}
