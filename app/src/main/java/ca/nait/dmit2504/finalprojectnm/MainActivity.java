db198cb95f4fa9f07bc5d0313a7f9324f222c6e6
geewhombee <geewhombee@gmail.ca>
2020-07-03 19:25:42 -0600

    floating button on click goes to adding new people activity. NewPeopleActivity layout complete, handle actual insert method next

--- app/src/main/java/ca/nait/dmit2504/finalprojectnm/MainActivity.java
+++ app/src/main/java/ca/nait/dmit2504/finalprojectnm/MainActivity.java
@@ -3,7 +3,11 @@ package ca.nait.dmit2504.finalprojectnm;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton addNewFloatingButton = findViewById(R.id.floatingActionButton);
+        addNewFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addNewIntent = new Intent(getApplicationContext(),NewPeopleActivity.class);
+                startActivity(addNewIntent);
+            }
+        });
         String test = "test";
 
         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
---
+++ app/src/main/java/ca/nait/dmit2504/finalprojectnm/NewPeopleActivity.java
@@ -0,0 +1,14 @@
+package ca.nait.dmit2504.finalprojectnm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewPeopleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_people);
+    }
+}
