aa9a0bf70a49072f0e0b1fe3348c93d3f633b418
geewhombee <geewhombee@gmail.ca>
2020-07-04 19:40:14 -0600

    added some comments

--- app/src/main/java/ca/nait/dmit2504/finalprojectnm/AppDatabase.java
+++ app/src/main/java/ca/nait/dmit2504/finalprojectnm/AppDatabase.java
@@ -6,20 +6,20 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {People.class}, version = 3, exportSchema = false)
+@Database(entities = {People.class}, version = 3, exportSchema = false)//add exportSchema to stop a debug warning
 public abstract class AppDatabase extends RoomDatabase {
 
     public abstract PeopleDao peopleDao();
 
     private static volatile AppDatabase appDatabase;
-
+    //
     static AppDatabase getDatabase(final Context context) {
         if (appDatabase == null) {
             synchronized (AppDatabase.class) {
                 if (appDatabase == null) {
                     appDatabase = Room.databaseBuilder(context.getApplicationContext(),
                             AppDatabase.class, "user_database")
-                            .allowMainThreadQueries()
+                            .allowMainThreadQueries() //this allows database usage on the Main thread. This is not best practise, and should be replaced.
                             .fallbackToDestructiveMigration()
                             .build();
                 }
